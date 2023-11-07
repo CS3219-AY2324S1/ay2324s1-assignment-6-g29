03df21b8f82dc99d3ff4fc9a5512e7f0526bd9dd
benedictchuajj <benedictchuajj@gmail.com>
2023-11-07 21:59:18 +0800

    Integrate CRUD features for user database

--- assignment-1/frontend-crud/src/screens/QuestionTable.js
+++ assignment-1/frontend-crud/src/screens/QuestionTable.js
@@ -1,188 +1,189 @@
 import { Input, Table, Space, Tag } from 'antd'
 import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
 import EditQuestionButton from '../buttons/EditQuestionButton'
 import CreateQuestionButton from '../buttons/CreateQuestionButton'
 import DeleteQuestionButton from '../buttons/DeleteQuestionButton'
 import ViewMoreButton from '../buttons/ViewMoreButton'
 import { SearchOutlined } from '@ant-design/icons'
 import QuestionService from '../service/QuestionService'
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid'
 
-const QuestionTable = ({questions, setQuestions}) => {
-  const [filteredQuestions, setFilteredQuestions] = useState([]);
+const QuestionTable = ({ questions, setQuestions }) => {
+  const [filteredQuestions, setFilteredQuestions] = useState([])
 
-    // retrieving data from books.json
-    useEffect(() => {
-        QuestionService
-            .getAll()
-            .then(questions => {
-                setQuestions(questions); 
-            })
-    }, [setQuestions])
+  // retrieving data from books.json
+  useEffect(() => {
+    QuestionService
+      .getAll()
+      .then(questions => {
+        setQuestions(questions)
+      })
+  }, [setQuestions])
 
-    const handleFilterSearch = (column, value) => {
-      const filtered = questions.filter((question) =>
-        question[column].toLowerCase().includes(value.toLowerCase())
-      );
-      setFilteredQuestions(filtered);
-    };
+  const handleFilterSearch = (column, value) => {
+    const filtered = questions.filter((question) =>
+      question[column].toLowerCase().includes(value.toLowerCase())
+    )
+    setFilteredQuestions(filtered)
+  }
 
-    
-    // header for table + able to filter data using filterDropdown
-    const columns = [
-      {
-        title: "Id",
-        dataIndex: "id",
-        key: "id",
-      },
-      {
-        title: "Title",
-        dataIndex: "title",
-        key: "title",
-        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
-          return (
-            <>
-              <Input
-                autoFocus
-                id={`filter-title`}
-                placeholder="Type text here"
-                value={selectedKeys[0]}
-                onChange={(e) => {
-                  setSelectedKeys(e.target.value ? [e.target.value] : []);
-                  confirm({ closeDropdown: false });
-                }}
-                onPressEnter={() => {
-                  confirm();
-                }}
-                onBlur={() => {
-                  confirm();
-                }}
-              ></Input>
-            </>
-          );
-        },
-        filterIcon: () => {
-          return <SearchOutlined />;
-        },
-        onFilter: (value, record) => {
-          return record.title.toLowerCase().includes(value.toLowerCase());
-        },
-      },
-      {
-        title: "Description",
-        dataIndex: "description",
-        key: "description",
-        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
-          return (
-            <>
-              <Input
-                autoFocus
-                id={`filter-description`}
-                name={"description"}
-                placeholder="Type text here"
-                value={selectedKeys[0]}
-                onChange={(e) => {
-                  setSelectedKeys(e.target.value ? [e.target.value] : []);
-                  confirm({ closeDropdown: false });
-                }}
-                onPressEnter={() => {
-                  confirm();
-                }}
-                onBlur={() => {
-                  confirm();
-                }}
-              ></Input>
-            </>
-          );
-        },
-        filterIcon: () => {
-          return <SearchOutlined />;
-        },
-        onFilter: (value, record) => {
-          return record.description.toLowerCase().includes(value);
-        },
-      },
-      {
-        title: "Complexity",
-        dataIndex: "complexity",
-        key: "complexity",
-        filters: [
-          { text: "Hard", value: "Hard" },
-          { text: "Easy", value: "Easy" },
-          { text: "Medium", value: "Medium" },
-        ],
-        onFilter: (value, record) => record.complexity.includes(value),
-      },
-
-      {
-        title: "Tags",
-        dataIndex: "tags",
-        key: "tags",
-        render: (_, { tags }) => (
+  // header for table + able to filter data using filterDropdown
+  const columns = [
+    {
+      title: 'Id',
+      dataIndex: 'id',
+      key: 'id'
+    },
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
+        return (
           <>
-            {tags &&
+            <Input
+              autoFocus
+              id='filter-title'
+              placeholder='Type text here'
+              value={selectedKeys[0]}
+              onChange={(e) => {
+                setSelectedKeys(e.target.value ? [e.target.value] : [])
+                confirm({ closeDropdown: false })
+              }}
+              onPressEnter={() => {
+                confirm()
+              }}
+              onBlur={() => {
+                confirm()
+              }}
+            />
+          </>
+        )
+      },
+      filterIcon: () => {
+        return <SearchOutlined />
+      },
+      onFilter: (value, record) => {
+        return record.title.toLowerCase().includes(value.toLowerCase())
+      }
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
+        return (
+          <>
+            <Input
+              autoFocus
+              id='filter-description'
+              name='description'
+              placeholder='Type text here'
+              value={selectedKeys[0]}
+              onChange={(e) => {
+                setSelectedKeys(e.target.value ? [e.target.value] : [])
+                confirm({ closeDropdown: false })
+              }}
+              onPressEnter={() => {
+                confirm()
+              }}
+              onBlur={() => {
+                confirm()
+              }}
+            />
+          </>
+        )
+      },
+      filterIcon: () => {
+        return <SearchOutlined />
+      },
+      onFilter: (value, record) => {
+        return record.description.toLowerCase().includes(value)
+      }
+    },
+    {
+      title: 'Complexity',
+      dataIndex: 'complexity',
+      key: 'complexity',
+      filters: [
+        { text: 'Hard', value: 'Hard' },
+        { text: 'Easy', value: 'Easy' },
+        { text: 'Medium', value: 'Medium' }
+      ],
+      onFilter: (value, record) => record.complexity.includes(value)
+    },
+
+    {
+      title: 'Tags',
+      dataIndex: 'tags',
+      key: 'tags',
+      render: (_, { tags }) => (
+        <>
+          {tags &&
               tags.map((tag) => {
                 return (
-                  <Tag color="green" key={tag}>
+                  <Tag color='green' key={tag}>
                     {tag}
                   </Tag>
-                );
+                )
               })}
-          </>
-        ),
-        filters: [
-          { text: "Algorithms", value: "algorithms" },
-          { text: "Data Structures", value: "data structures" },
-          { text: "Recursion", value: "recursion" },
-          { text: "Bit Manipulation", value: "bit manipulation"}
-        ],
-        onFilter: (value, record) => record.tags.includes(value),
-      },
-      {
-        title: "Actions",
-        dataIndex: "actions",
-        key: "actions",
-        render: (text, record, index) => (
-          <Space>
-            <ViewMoreButton question={record}/>
-            <DeleteQuestionButton questions={questions} setQuestions={setQuestions} id={record.id} />
-            <EditQuestionButton question={record}/>
-          </Space>
-        ),
-      },
-    ];
+        </>
+      ),
+      filters: [
+        { text: 'Algorithms', value: 'algorithms' },
+        { text: 'Data Structures', value: 'data structures' },
+        { text: 'Recursion', value: 'recursion' },
+        { text: 'Bit Manipulation', value: 'bit manipulation' }
+      ],
+      onFilter: (value, record) => record.tags.includes(value)
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions',
+      render: (text, record, index) => (
+        <Space>
+          <ViewMoreButton question={record} />
+          <DeleteQuestionButton questions={questions} setQuestions={setQuestions} id={record.id} />
+          <EditQuestionButton question={record} />
+        </Space>
+      )
+    }
+  ]
 
-    // data to fill up the rows of the table
-    const data = questions.map(question => {
-        return (
-            {
-                id: question.id,
-                title: question.title,
-                description: question.description,
-                complexity: question.complexity, 
-                tags: question.tags,
-                key: uuidv4()
-            }
-        )
-    })
-
+  // data to fill up the rows of the table
+  const data = questions.map(question => {
     return (
-        <div style={{justifyContent: 'start', alignItems: "start"}}>
-            <div style={{display: 'flex'}}>
-                <h2 style={{paddingLeft: '1.5%', paddingTop: '1%'}}>
-                    Question Table
-                </h2>
-                <div style={{ marginLeft: 'auto', paddingRight: '1.5%', paddingTop: '1%' }}>
-                    {<CreateQuestionButton />}
-                </div>
-            </div>
-            <Table 
-                style={{padding: '1%'}}
-                dataSource={data} 
-                columns={columns} 
-            />
-        </div>
+      {
+        id: question.id,
+        title: question.title,
+        description: question.description,
+        complexity: question.complexity,
+        tags: question.tags,
+        key: uuidv4()
+      }
     )
+  })
+
+  return (
+    <div style={{ justifyContent: 'start', alignItems: 'start' }}>
+      <Navbar />
+      <div style={{ display: 'flex' }}>
+        <h2 style={{ paddingLeft: '1.5%', paddingTop: '1%' }}>
+          Question Table
+        </h2>
+        <div style={{ marginLeft: 'auto', paddingRight: '1.5%', paddingTop: '1%' }}>
+          <CreateQuestionButton />
+        </div>
+      </div>
+      <Table
+        style={{ padding: '1%' }}
+        dataSource={data}
+        columns={columns}
+      />
+    </div>
+  )
 }
 
 export default QuestionTable
---
+++ assignment-1/frontend-crud/src/screens/SignupPage.js
@@ -0,0 +1,191 @@
+// TODO: check if commented out code is needed
+
+import React, { useState } from 'react'
+import { Box, Button, Typography, TextField } from '@mui/material'
+import { useDispatch } from 'react-redux'
+import { setShowError, setErrorMessage } from '../redux/ErrorSlice.js'
+import {
+  setUserid,
+  setLoginStatus,
+  setIdToken
+} from '../redux/UserSlice.js'
+import { useNavigate } from 'react-router-dom'
+import LoginPageBanner from '../components/FrontPageBanner.js'
+import axios from 'axios'
+
+function SignupPage () {
+  const [name, setName] = useState('')
+
+  const [username, setUsername] = useState('')
+
+  const [email, setEmail] = useState('')
+
+  const [password, setPassword] = useState('')
+
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+  const [passwordConfirmationError, setPasswordConfirmationError] = useState('')
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const requireAllNonNull = () => {
+    const requiredFields = [
+      name,
+      username,
+      email,
+      password,
+      passwordConfirmation
+    ]
+    requiredFields.forEach((x, i) => {
+      if (x === '') {
+        throw new Error('All fields cannot be empty')
+      }
+    })
+  }
+
+  const checkPasswords = (password1, password2) => {
+    if (password1 !== password2) {
+      setPasswordConfirmationError('Passwords do not match.')
+      throw new Error('Passwords do not match')
+    } else {
+      setPasswordConfirmationError('')
+    }
+  }
+
+  const handleSignUp = async (e) => {
+    e.preventDefault()
+
+    try {
+      requireAllNonNull()
+      checkPasswords(password, passwordConfirmation)
+      await axios.post('http://localhost:5000/addUser', {
+        name,
+        username,
+        email,
+        password,
+        role: 'User'
+      })
+      console.log('sign up success')
+      const tokenCredential = await axios.post('http://localhost:5000/login', {
+        username,
+        password
+      })
+      dispatch(setUserid(username))
+      dispatch(setIdToken(tokenCredential))
+      console.log('Login successful')
+      dispatch(setLoginStatus(true))
+      navigate('/questions')
+    } catch (error) {
+      dispatch(setErrorMessage(error.message))
+      dispatch(setShowError(true))
+    }
+  }
+
+  const routeChangeSignin = () => {
+    const path = '/login'
+    navigate(path)
+  }
+
+  return (
+    <Box display='flex' flexDirection='row' height='100vh'>
+      <Box
+        display='flex'
+        flex={1}
+        flexDirection='column'
+        justifyContent='center'
+      >
+        <Box display='flex' flexDirection='column' padding='25%'>
+          <Typography variant='h4' marginBottom='1rem' fontWeight='bold'>
+            First step to landing your dream job
+          </Typography>
+          <Typography variant='body2' marginBottom='2rem'>
+            <b>Please enter your details</b>
+          </Typography>
+          <form
+            onSubmit={handleSignUp}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column'
+            }}
+          >
+            <TextField
+              type='text'
+              placeholder='Display Name'
+              variant='standard'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              sx={{ marginBottom: '1rem' }}
+              fullWidth
+              required
+            />
+            <TextField
+              type='text'
+              placeholder='Username'
+              variant='standard'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              sx={{ marginBottom: '1rem' }}
+              fullWidth
+              required
+            />
+            <TextField
+              type='email'
+              placeholder='Email'
+              variant='standard'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              sx={{ marginBottom: '1rem' }}
+              fullWidth
+              required
+            />
+            <TextField
+              type='password'
+              placeholder='Password'
+              variant='standard'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              sx={{ marginBottom: '1rem' }}
+              fullWidth
+              required
+            />
+            <TextField
+              type='password'
+              placeholder='Confirm Password'
+              variant='standard'
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+              sx={{ marginBottom: '1rem' }}
+              error={passwordConfirmationError !== ''}
+              helperText={passwordConfirmationError}
+              fullWidth
+              required
+            />
+            <Button variant='contained' type='submit' fullWidth>
+              <b>Sign up</b>
+            </Button>
+          </form>
+          <Typography variant='body2' marginTop='1rem'>
+            Already have an account?
+            <a
+              style={{
+                marginLeft: '0.5em',
+                fontWeight: 'bolder',
+                textDecoration: 'none',
+                color: '#1976d2'
+              }}
+              href=''
+              onClick={routeChangeSignin}
+            >
+              Sign in
+            </a>
+          </Typography>
+        </Box>
+      </Box>
+      <LoginPageBanner />
+    </Box>
+  )
+}
+
+export default SignupPage
